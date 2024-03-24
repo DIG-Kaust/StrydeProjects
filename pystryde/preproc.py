f9387c8917205b5dddc8ad74dccd4b5e8d9b4bc9
mrava87 <matteoravasi@gmail.com>
2024-03-24 12:30:11 +0300

    Added correct_sensor_response method

--- pystryde/preproc.py
+++ pystryde/preproc.py
@@ -2,9 +2,32 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
-from scipy.signal import butter, sosfiltfilt, correlate
+from scipy.signal import butter, sosfiltfilt, correlate, filtfilt
 from pylops.signalprocessing import Shift
 from pystryde.visual import wiggletracecomb
+
+
+def mag2db(x):
+    """Magnitude to Decibel"""
+    return 20 * np.log10(x)
+
+
+def db2mag(x):
+    """Decibel to Magnitude"""
+    return 10 ** (x / 20)
+
+
+def customplot(ax, x, y, color, plottype):
+    """Customized plotting
+
+    Add plot to a given axis based on user-defined plottype
+    """
+    if plottype == plt.plot:
+        ax.plot(x, y, color)
+    elif plottype == plt.semilogy:
+        ax.semilogy(x, y, color)
+    elif plottype == plt.loglog:
+        ax.loglog(x, y, color)
 
 
 def filterdata(nfilt, fmin, fmax, dt, inp):
@@ -228,3 +251,153 @@ def aligndata(data, irec, t, tlims, ishotmaster=0, otherdata=None, plotflag=Fals
     return datashift, (fig, fig1, fig2, fig3, fig4, fig5, fig6) if plotflag else None
 
 
+def correct_sensor_response(data, sensor_response, t_response,
+                            nfft=None, fcorners=None, nsmooth=1, 
+                            waterlevel=60, waterlevelkind='add',
+                            plotflag=False, plottype=plt.plot):
+    """Correct sensor response
+
+    Apply correction to data based on sensor response. Optionally, one can choose to flatten
+    the response in the entire bandwidth of the acquired signal, or in a selected bandwidth.
+
+    The sensor correction process is carried out in the frequency domain in the following 3 steps:
+
+    - A flat frequency spectrum is created based on ``fcorners`` amd ``nsmooth`` parameters, which
+      will be used as target output for the sensor correction
+    - A stable inverse response is created for sensor response using a user-defined ``waterlevel``
+    - The product of the output frequency spectrum and inverse sensor respose are applied to the
+      frequency-domain version of the input data
+
+    Two options can be chosen for the inverse:
+
+    - ``waterlevelkind=add``: the water level is added to the denominator of the inverse response
+    - ``waterlevelkind=thresh``: the water level is used as threshold and set for any frequency whose response is
+        below this value.
+
+    Parameters
+    ----------
+    data : :obj:`numpy.ndarray`
+        Data of size `nr x nt`
+    sensor_response : :obj:`numpy.ndarray`
+        Sensor response of size `ntresp`
+    t_response : :obj:`numpy.ndarray`
+        Time axis of sensor response of size `ntresp`
+    nfft : :obj:`int`, optional
+        Number of samples in the Fourier domain
+    fcorners : :obj:`tuple` or :obj:`list`, optional
+        Corner frequencies (``flow, fmidlow, fmidhigh, fhigh``) used to create the ideal output response
+        with 0 before ``flow`` and after ``fhigh``, 1 in between ``fmidlow, fmidhigh``, and linear ramps
+        from ``flow`` to ``fmidlow`` and from ``fmidhigh`` to ``fhigh``
+    nsmooth : :obj:`int`, optional
+        Number of samples for smoothing of the ideal output response
+    waterlevel : :obj:`float`, optional
+        Water level in dB added to inverse of sensor response (note, if X is
+        chosen this will be will be -XdB of peak amplitude)
+    waterlevelkind :obj:`str`, optional
+        Water level kind (``add`` or ``thresh``). See above for details.
+    plotflag : :obj:`bool`, optional
+        Plotting flag
+    plottype : :obj:`matplotlib.pyplot`, optional
+        Plotting method used for ploting
+
+    Returns
+    -------
+    datafilt : :obj:`numpy.ndarray`
+        Filtered data of size `nr x nt`
+    OUTeffective : :obj:`numpy.ndarray`
+        Effective output response (response divided by its shaped inverse)
+    f : :obj:`numpy.ndarray`
+        Frequency axis
+
+    """
+    nr, nt = data.shape
+    dt = t_response[1] - t_response[0]
+    t = np.arange(nt) * dt
+
+    if nfft is None:
+        nfft = nt
+
+    # find center of sensor response from time axis
+    itcenter = np.argmin(np.abs(t_response))
+
+    # compute and remove mean from data
+    data_mean = np.mean(data)
+    data -= data_mean
+
+    # convert response and data to frequency domain
+    f = np.fft.rfftfreq(nfft, dt)
+    df = f[1]
+
+    Response = np.fft.rfft(sensor_response, n=nfft)
+    D = np.fft.rfft(data, n=nfft, axis=-1)
+
+    # design output frequency spectrum
+    OUT = np.ones_like(f)
+    if fcorners is not None:
+        ifcorners = [None if fcorner is None else int(np.round(fcorner / df)) for fcorner in fcorners]
+        if ifcorners[0] is not None:
+            OUT[:ifcorners[0]] = 0.
+            OUT[ifcorners[0]:ifcorners[1]] = np.arange(ifcorners[1] - ifcorners[0]) / (ifcorners[1] - ifcorners[0])
+        if ifcorners[3] is not None:
+            OUT[ifcorners[2]:ifcorners[3]] = np.arange(ifcorners[3] - ifcorners[2], 0, -1) / (
+                        ifcorners[3] - ifcorners[2])
+            OUT[ifcorners[3]:] = 0.
+        if nsmooth > 1:
+            OUT = filtfilt(np.ones(nsmooth) / nsmooth, 1, OUT)
+
+    # define water level
+    waterlevel = db2mag(mag2db(np.abs(Response).max()) - waterlevel)
+
+    # compute inverse response
+    if waterlevelkind == 'add':
+        Response_inv = np.conj(Response) / (np.conj(Response) * Response + waterlevel)
+    elif waterlevelkind == 'thresh':
+        Response_inv = 1. / Response
+        Response_mask = np.abs(Response) < waterlevel
+        Response_inv[Response_mask] = waterlevel / np.exp(1j*np.angle(Response)[Response_mask])
+    else: 
+        raise NotImplementedError('waterlevelkind must be add or threshold...')
+
+    Response_inv_shaped = Response_inv * OUT
+
+    # compute effective output response
+    OUTeffective = np.abs(Response * Response_inv_shaped)
+
+    # apply inverse response to data
+    Sop = Shift((nr, nfft), itcenter * dt, axis=-1, sampling=dt, real=True)
+    Dfilt = (Response_inv_shaped * D)
+    datafilt = (Sop @ np.real(np.fft.irfft(Dfilt, axis=-1)))[..., :nt]
+
+    # add mean back
+    datafilt += data_mean
+
+    if plotflag:
+
+        fig, axs = plt.subplots(2, 2, figsize=(18, 8))
+        customplot(axs[0, 0], f, np.abs(D[nr // 2]), 'k', plottype)
+        axs[0, 0].set_ylabel('Raw data spectrum')
+        ax2 = axs[0, 0].twinx()
+        ax2.plot(f, OUT, 'r')
+        ax2.set_ylabel('Desired sensor response', color='r')
+        ax2.tick_params(axis='y', labelcolor='r')
+
+        customplot(axs[0, 1], f, OUT * np.abs(D[nr // 2]), 'k', plottype)
+        axs[0, 1].set_ylabel('Filtered data spectrum')
+        ax2 = axs[0, 1].twinx()
+        customplot(ax2, f, np.abs(Response), 'r', plottype)
+        ax2.set_ylabel('Sensor Response', color='r')
+        ax2.tick_params(axis='y', labelcolor='r')
+
+        customplot(axs[1, 0], f, np.abs(Dfilt[nr // 2]), 'k', plottype)
+        axs[1, 0].set_ylabel('Sensor corrected data spectrum')
+        ax2 = axs[1, 0].twinx()
+        customplot(ax2, f, np.abs(Response_inv_shaped), 'r', plottype)
+        ax2.set_ylabel('Inverse Sensor Response', color='r')
+        ax2.tick_params(axis='y', labelcolor='r')
+
+        axs[1, 1].plot(t, data[nr // 2], 'k', lw=2)
+        axs[1, 1].plot(t, datafilt[nr // 2], 'r', lw=1)
+        axs[1, 1].set_ylabel('Time domain data')
+        plt.tight_layout()
+
+    return datafilt, OUTeffective, f
